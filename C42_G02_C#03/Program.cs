758229ec8730004aeab13cd9d83489eda2aabbec
Youssef Khalil <youkhalil@microsoft.com>
2024-12-12 08:42:47 +0200

    Assignment #3

---
+++ C42_G02_C#03/Program.cs
@@ -0,0 +1,187 @@
+﻿namespace C42_G02_C_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region Q1
+            // Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            //Console.WriteLine("Enter A Number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if (num % 3 == 0 && num % 4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+            ////#endregion
+
+            //#region Q2
+            // Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+            //Console.WriteLine("Enter A Number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if (num<0)
+            //{
+            //    Console.WriteLine("Negative");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Positive");
+            //}
+            //#endregion
+
+            //#region Q3
+            // Write a program that takes 3 integers from the user then prints the max element and the min element.
+            //Console.WriteLine("Enter A Number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+            //int max = Math.Max(num1, Math.Max(num2, num3));
+            //int min = Math.Min(num1, Math.Min(num2, num3));
+            //Console.WriteLine($"Max element : {max}");
+            //Console.WriteLine($"Min element : {min}");
+            //#endregion
+
+            //#region Q4
+            //Write a program that allows the user to insert an integer number then check If a number is even or odd.
+            //Console.WriteLine("Enter A Number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if (num % 2 == 0)
+            //{
+            //    Console.WriteLine("Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd");
+            //}
+            //#endregion
+
+            //#region Q5
+            //Console.WriteLine("Enter A Character");
+            //char ch = Convert.ToChar(Console.ReadLine());
+            //ch = char.ToLower(ch);
+            //if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            //{
+            //    Console.WriteLine("Vowel");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Consonant");
+            //}
+            //#endregion
+
+            //#region Q6
+            ////Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+            //Console.WriteLine("Enter A Number");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //while(n!=0)
+            //{
+            //    Console.Write(n + " ");
+            //    n--;
+            //}
+            //#endregion
+
+            //#region Q7
+            //// Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+            //Console.WriteLine("Enter A Number");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    Console.Write(n * i + " ");
+            //}
+            //#endregion
+
+            //#region Q8
+            //Write a program that allows to user to insert number then print all even numbers between 1 to this number
+            //Console.WriteLine("Enter A Number");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    if((i&1)==0)
+            //        Console.WriteLine(i);
+            //}
+            //#endregion
+
+            //#region Q9
+            //// Write a program that takes two integers then prints the power.
+            //Console.WriteLine("Enter two integers");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(Math.Pow(num1, num2));
+            //#endregion
+
+            //#region Q10
+            //// Write a program to enter marks of five subjects and calculate total, average and percentage.
+            //Console.WriteLine("Enter Marks of 5 Subjects");
+            //int sub1 = Convert.ToInt32(Console.ReadLine());
+            //int sub2 = Convert.ToInt32(Console.ReadLine());
+            //int sub3 = Convert.ToInt32(Console.ReadLine());
+            //int sub4 = Convert.ToInt32(Console.ReadLine());
+            //int sub5 = Convert.ToInt32(Console.ReadLine());
+            //int total = sub1 + sub2 + sub3 + sub4 + sub5;
+            //double average = total / 5.0;
+            //double percentage = (total / 500.0) * 100;
+            //Console.WriteLine($"Total : {total}");
+            //Console.WriteLine($"Average : {average}");
+            //Console.WriteLine($"Percentage : {percentage}");
+            //#endregion
+
+            //#region Q11
+            // Write a program to input the month number and print the number of days in that month.
+
+            //Console.WriteLine("Enter Month Number");
+            //int month = Convert.ToInt32(Console.ReadLine());
+            //switch (month)
+            //{
+            //    case 1:
+            //    case 3:
+            //    case 5:
+            //    case 7:
+            //    case 8:
+            //    case 10:
+            //    case 12:
+            //        Console.WriteLine("31 Days");
+            //        break;
+            //    case 4:
+            //    case 6:
+            //    case 9:
+            //    case 11:
+            //        Console.WriteLine("30 Days");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("28 Days");
+            //        break;
+            //}
+            //#endregion
+
+            //#region Q12
+            ////Write a program to create a Simple Calculator.
+            //Console.WriteLine("Enter First Number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Second Number");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Operation (+, -, *, /)");
+            //char op = Convert.ToChar(Console.ReadLine());
+            //switch (op)
+            //{
+            //    case '+':
+            //        Console.WriteLine(num1 + num2);
+            //        break;
+            //    case '-':
+            //        Console.WriteLine(num1 - num2);
+            //        break;
+            //    case '*':
+            //        Console.WriteLine(num1 * num2);
+            //        break;
+            //    case '/':
+            //        Console.WriteLine(num1 / (double)num2);
+            //        break;
+            //}
+            //#endregion
+
+        }
+
+    }
+    }
