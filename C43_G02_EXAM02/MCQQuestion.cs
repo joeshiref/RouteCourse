867b9f92ac647a5082f7cd235464b305c70be5ec
Youssef Khalil <youkhalil@microsoft.com>
2025-01-25 22:59:53 +0200

    Removing unnecessary conditions

--- C43_G02_EXAM02/MCQQuestion.cs
+++ C43_G02_EXAM02/MCQQuestion.cs
@@ -9,14 +9,7 @@ namespace C43_G02_EXAM02
     internal class MCQQuestion : Question
     {
         public MCQQuestion(string header, string body, decimal mark, List<Answer> answers, Answer rightAnswer)
-        : base(header, body, mark, answers, rightAnswer)
-        {
-            //if (answers.Count < 2)
-            //    throw new ArgumentException("MCQ must have at least 2 answers");
-
-            //if (!answers.Contains(rightAnswer))
-            //    throw new ArgumentException("Right answer must be in answer list");
-        }
+        : base(header, body, mark, answers, rightAnswer) { }
 
         public override object Clone() => new MCQQuestion(
             Header,
