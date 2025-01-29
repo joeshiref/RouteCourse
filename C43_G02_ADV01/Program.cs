f3659ce42023174216d74818465f754b3897f5ff
Youssef Khalil <youkhalil@microsoft.com>
2025-01-30 00:15:46 +0200

    Advance C# Assignment #1

---
+++ C43_G02_ADV01/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace C43_G02_ADV01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            // The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm And implement the code of this optimized bubble sort algorithm
+            // We can add a flag to check if the array is already sorted or not.
+            // If the array is already sorted, then there is no need to continue;
+            //int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            //int n = arr.Length;
+            //bool swapped;
+            //for (int i = 0; i < n - 1; i++)
+            //{
+            //    swapped = false;
+            //    for (int j = 0; j < n - i - 1; j++)
+            //    {
+            //        if (arr[j] > arr[j + 1])
+            //        {
+            //            int temp = arr[j];
+            //            arr[j] = arr[j + 1];
+            //            arr[j + 1] = temp;
+            //            swapped = true;
+            //        }
+            //    }
+            //    if (!swapped)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            #endregion
+
+            #region Question 2
+
+            //var intRange = new Range<int>(5, 10);
+            //Console.WriteLine(intRange.IsInRange(7));
+            //Console.WriteLine(intRange.Length());
+
+
+            #endregion
+        }
+    }
+}
---
+++ C43_G02_ADV01/Range.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C43_G02_ADV01
+{
+    public class Range<T> where T : IComparable<T>
+    {
+        public T Min { get; }
+        public T Max { get; }
+
+        public Range(T min, T max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+
+        public double Length()
+        {
+            dynamic min = Min;
+            dynamic max = Max;
+            return (double)(max - min);
+        }
+    }
+}
